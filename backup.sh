a2142098cb84fad6902cc434933f6d7894586821
Victor Engmark <victor.engmark@gmail.com>
2011-09-06 22:50:16 +0200

    Backup script

---
+++ backup.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# NAME
+#    backup.sh - Create a backup of your Minecraft main directory
+#
+# SYNOPSIS
+#    ./backup.sh
+#
+# DESCRIPTION
+#    Moves ~/.minecraft to ~/.minecraft-<datetime>.
+#
+#    If your Minecraft directory for some reason is not in your home directory,
+#    then modify minecraft_home in minecraft-scripts.conf.
+#
+# BUGS
+#    https://github.com/l0b0/minecraft-scripts/issues
+#
+# COPYRIGHT AND LICENSE
+#    Copyright (C) 2011 Victor Engmark
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+set -o errexit -o noclobber -o nounset -o pipefail
+
+. "$(dirname -- "$0")/minecraft-scripts.conf"
+
+if [[ ! -d "$minecraft_home" ]]
+then
+    echo "$minecraft_home does not exist; did you back it up already?" >&2
+    exit 1
+fi
+
+minecraft_backup="${minecraft_home}-$(date +%Y-%m-%dT%H:%M:%S)"
+mv -T -- "$minecraft_home" "$minecraft_backup"
+echo "Moved Minecraft folder to $minecraft_backup"
